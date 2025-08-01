922502bba4a033639a9ad81545e03b4d149a360b
Bhaumik senwal <bhaumiksenwal99@gmail.com>
2025-08-01 23:59:19 +0530

    Update 153.java

--- 153.java
+++ 153.java
@@ -1,1 +1,20 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int left = 0, right = nums.length - 1;
 
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            // If mid element is greater than right, minimum must be on right side
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                // Minimum could be at mid or to the left
+                right = mid;
+            }
+        }
+
+        // When loop ends, left == right and pointing to the smallest element
+        return nums[left];
+    }
+}
