795ee87eb5e9b917fec581ed507fed8c1ba163e0
Bhaumik senwal <bhaumiksenwal99@gmail.com>
2025-08-03 22:17:41 +0530

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,1 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSumSolution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();  // value -> index
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {};  // Should never happen as per problem statement
+    }
+
+    public static void main(String[] args) {
+        TwoSumSolution solution = new TwoSumSolution();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = solution.twoSum(nums, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+    }
+}
 
