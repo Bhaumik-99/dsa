b868e35a0eae64d8149fd77bd41744128bfc5298
Bhaumik senwal <bhaumiksenwal99@gmail.com>
2025-08-04 23:48:38 +0530

    Update 3.java

--- 3.java
+++ 3.java
@@ -1,1 +1,12 @@
-
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int[] index = new int[128];
+        int left = 0, maxLen = 0;
+        for (int right = 0; right < s.length(); right++) {
+            left = Math.max(index[s.charAt(right)], left);
+            maxLen = Math.max(maxLen, right - left + 1);
+            index[s.charAt(right)] = right + 1;
+        }
+        return maxLen;
+    }
+}
