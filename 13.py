b6b79b7dd61fee081ce3e06322f054d697e243d4
Bhaumik senwal <bhaumiksenwal99@gmail.com>
2025-08-19 00:00:02 +0530

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,1 +1,31 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        total = 0
+        prev_value = 0
+
+        # Traverse from right to left
+        for char in reversed(s):
+            value = roman_map[char]
+
+            if value < prev_value:
+                total -= value  # Subtractive case (like IV, IX, XL, etc.)
+            else:
+                total += value  # Normal case
+            prev_value = value
+
+        return total
 
