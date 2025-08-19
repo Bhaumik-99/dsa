d74e2d4e0b708828ef66300dcfd65e0c42cb6773
Bhaumik senwal <bhaumiksenwal99@gmail.com>
2025-08-19 23:57:36 +0530

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,1 +1,25 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(height) - 1
+        max_area = 0
+
+        while left < right:
+            # Calculate area
+            width = right - left
+            h = min(height[left], height[right])
+            area = width * h
+            max_area = max(max_area, area)
+
+            # Move the smaller line
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_area
+
 
