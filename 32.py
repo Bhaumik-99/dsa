c21c6f80296da79402f397b239c3dd5938a89aac
Bhaumik senwal <bhaumiksenwal99@gmail.com>
2025-09-02 23:58:36 +0530

    Update 32.py

--- 32.py
+++ 32.py
@@ -1,1 +1,21 @@
+class Solution(object):
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        stack = [-1]   # Initialize with -1 as base for valid substring
+        max_len = 0
+
+        for i, ch in enumerate(s):
+            if ch == "(":
+                stack.append(i)   # Push index of '('
+            else:
+                stack.pop()       # Pop last '(' index
+                if not stack:
+                    stack.append(i)  # Reset base index
+                else:
+                    max_len = max(max_len, i - stack[-1])  # Update max length
+
+        return max_len
 
